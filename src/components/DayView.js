becf1477749a874d7ea419d5d68c4bd6f84a526d
Shujaat Azim <shujaat.azim@gmail.com>
2019-11-10 19:15:30 -0500

    styling boxes

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -25,10 +25,14 @@ class DayView extends React.Component {
           <h4>October {currentDate.date}</h4>
         </div>
         <div>
-            <input type="checkbox" checked={currentDate.learning} onClick={() => this.props.toggleActivity('learning')} onChange={() => ""}/>Learning{" "}
-            <input type="checkbox" checked={currentDate.project} onClick={() => this.props.toggleActivity('project')} onChange={() => ""} />Current Project{" "}
-            <input type="checkbox" checked={currentDate.algorithms} onClick={() => this.props.toggleActivity('algorithms')} onChange={() => ""} />Algorithms{" "}
-            <input type="checkbox" checked={currentDate.networking} onClick={() => this.props.toggleActivity('networking')} onChange={() => ""} />Networking{" "}
+            <input type="checkbox" checked={currentDate.learning} onClick={() => this.props.toggleActivity('learning')} 
+            onChange={() => ""}/>Learning{" "}
+            <input type="checkbox" checked={currentDate.project} onClick={() => this.props.toggleActivity('project')} 
+            onChange={() => ""} />Current Project{" "}
+            <input type="checkbox" checked={currentDate.algorithms} onClick={() => this.props.toggleActivity('algorithms')} 
+            onChange={() => ""} />Algorithms{" "}
+            <input type="checkbox" checked={currentDate.networking} onClick={() => this.props.toggleActivity('networking')} 
+            onChange={() => ""} />Networking{" "}
         </div>
           <div>
             <br />
