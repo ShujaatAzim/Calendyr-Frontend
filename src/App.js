6ff58e1e5f78c5fe034918b77da15282c6f085c7
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-30 21:02:43 -0400

    Completely rebuilt app component to show a calendar, and then built a date viewer component which currently shows which date the parent component has selected. This will eventually hold the activities to be done on each day. Will probably separate from app component soon. Also customized the react calendar component with props.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DateView from './components/DateView';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const App = () => {
+  
+  const [date, setDate] = useState(new Date())
+  const [dateNumber, setDateNumber] = useState(null)
+  const [dateMonth, setDateMonth] = useState(null)
+
+  useEffect(() => {
+    let number = (date.getDate() < 10 ? '0' : '') + date.getDate()
+    let month = date.toLocaleString('default', { month: 'long' })
+    setDateNumber(number)
+    setDateMonth(month)
+  }, [date])
+
+  const changeDate = date => {
+    setDate(date)
+  }
+
   return (
     <div>
-      Hi
+      <Calendar calendarType="US" onChange={changeDate} value={date} />
+      <DateView date={dateNumber} month={dateMonth} />
     </div>
   )
 };
---
+++ src/components/DateView.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+
+const DateView = props => {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div>
+      {props.month}
+      {props.date}
+    </div>
+  )
+}
+
+export default DateView
