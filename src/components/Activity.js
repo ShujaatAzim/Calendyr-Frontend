edcff35c2c790c6eb0c3734a421c43df41ecd1b0
Shujaat Azim <shujaat.azim@gmail.com>
2020-06-06 23:54:47 -0400

    Changed the checkbox property to see if the object is already completed on the backend.

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Activity = props => {
   return (
     <>
-      <input type="checkbox" value={props.activity.complete} />
+      <input type="checkbox" checked={props.activity.complete} />
       <label>{props.activity.name}</label>
     </>
   )
