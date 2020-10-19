34498f91820588dfb2f46812f8e071abcf3bbf77
Shujaat Azim <shujaat.azim@gmail.com>
2020-10-18 23:32:41 -0400

    Going to need to uncheck the boxes, obv.

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Activity = props => {
+
+  const handleClick = e => {
+    console.log(e.target.value)
+  }
   return (
     <>
-      <input type="checkbox" checked={props.activity.complete} />
+      <input type="checkbox" checked={props.activity.complete} onChange={(e) => handleClick(e)}/>
       <label>{props.activity.name}</label>
     </>
   )
